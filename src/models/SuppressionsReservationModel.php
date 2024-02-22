4932459b4941a15762a95934e43ba26259199beb
maxousql <maxlaiyio@outlook.fr>
2024-02-22 21:22:41 +0100

    delete event

--- src/models/SuppressionsReservationModel.php
+++ src/models/SuppressionsReservationModel.php
@@ -6,22 +6,20 @@ class SuppressionsReservationModel
 {
     public function processsuppressionreservation()
     {
-    global $pdo;   
+        global $pdo;
 
-    $id_billet = $_GET['id_billet'];
+        $id_billet = $_GET['id_billet'];
 
-        
-    $stmt = $pdo->prepare("SELECT id_event FROM billet WHERE id_billet = ?");
-    $stmt->execute([$id_billet]);
-    $event_id = $stmt->fetchColumn();
 
-    $pdo->query("DELETE FROM billet WHERE id_billet = $id_billet");
+        $stmt = $pdo->prepare("SELECT id_event FROM billet WHERE id_billet = ?");
+        $stmt->execute([$id_billet]);
+        $event_id = $stmt->fetchColumn();
 
-    $pdo->query("UPDATE event SET nombre_de_participants = nombre_de_participants - 1 WHERE id_event = $event_id");
+        $pdo->query("DELETE FROM billet WHERE id_billet = $id_billet");
 
-    header("Location: mesreservation");
-    exit();
+        $pdo->query("UPDATE event SET nombre_de_participants = nombre_de_participants - 1 WHERE id_event = $event_id");
+
+        header("Location: mesreservation");
+        exit();
     }
 }
-
-?>
