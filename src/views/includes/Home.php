0438743c3cf487b6f7dabd74eb3886c7651e606d
maxousql <maxlaiyio@outlook.fr>
2024-02-22 22:26:22 +0100

    changement requete profile

--- src/views/includes/Home.php
+++ src/views/includes/Home.php
@@ -1,221 +1,193 @@
-<!DOCTYPE html>
-<html>
+   <header>
+       <div>
+           <h1 class="tt bleu">BEEDE</h1>
+           <h2 class="tt">- BDE du campus sciences-u Lyon ! Bienvenue dans la ruche !! BZZ BZZ -</h2>
+       </div>
+   </header>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BEEDE</title>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;1,900&display=swap"
-        rel="stylesheet">
+   <main>
 
-        
+       <section id="programs" class="container">
+           <div class="centered-images">
+               <a href="#" style="background-image: url('img/soiree.jpg');">
+                   <h2 class="br">Soirée au Loft</h2>
+               </a>
+               <a href="#" style="background-image: url('img/course.jpg');">
+                   <h2 class="br">Sortie course à pieds</h2>
+               </a>
+           </div>
+       </section>
+       <!-- fin section #programs -->
 
-</head>
+       <section id="objectives">
+           <div class="container">
+               <h2 class="br bleu">Nos meilleures sorties :</h2>
+               <p class="br bleu">Ici vous retrouverez nos actions en vedette:</p>
+           </div>
 
-<body class="bgc">
+           <!-- fin section #objectives -->
 
-    <header>
-        <div>
-            <h1 class="tt bleu">BEEDE</h1>
-            <h2 class="tt">- BDE du campus sciences-u Lyon ! Bienvenue dans la ruche !! BZZ BZZ -</h2>
-        </div>
-    </header>
+           <!-- Début card -->
+           <div class="imgc">
+               <div class="card-hover">
+                   <div class="card-hover__content">
+                       <h3 class="card-hover__title">
+                           Azard Carnavage Hosted by <span>DJ MARTIN</span>
+                       </h3>
+                       <p class="card-hover__text">Préparez-vous à vous envoler dans l'univers de notre nouvelle soirée
+                           astronomique au AZARD</p>
+                       <a href="/events" class="card-hover__link">
+                           <span>Réserver</span>
+                           <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                               <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                           </svg>
+                       </a>
+                   </div>
+                   <div class="card-hover__extra">
+                       <h4>Rendez-vous le 9 <span>now</span> and get <span>40%</span> discount!</h4>
+                   </div>
+                   <img src="img/cartes.jpg" alt="">
+               </div>
+               <div class="card-hover">
+                   <div class="card-hover__content">
+                       <h3 class="card-hover__title">
+                           Make your <span>choice</span> right now!
+                       </h3>
+                       <p class="card-hover__text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia
+                           quisquam
+                           doloremque nostrum laboriosam, blanditiis libero corporis nulla a aut?</p>
+                       <a href="/events" class="card-hover__link">
+                           <span>Learn How</span>
+                           <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                               <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                           </svg>
+                       </a>
+                   </div>
+                   <div class="card-hover__extra">
+                       <h4>Learn <span>now</span> and get <span>40%</span> discount!</h4>
+                   </div>
+                   <img src="img/cartes.jpg" alt="">
+               </div>
+               <div class="card-hover">
+                   <div class="card-hover__content">
+                       <h3 class="card-hover__title">
+                           Make your <span>choice</span> right now!
+                       </h3>
+                       <p class="card-hover__text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia
+                           quisquam
+                           doloremque nostrum laboriosam, blanditiis libero corporis nulla a aut?</p>
+                       <a href="/events" class="card-hover__link">
+                           <span>Learn How</span>
+                           <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                               <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                           </svg>
+                       </a>
+                   </div>
+                   <div class="card-hover__extra">
+                       <h4>Learn <span>now</span> and get <span>40%</span> discount!</h4>
+                   </div>
+                   <img src="img/cartes.jpg" alt="">
+               </div>
+           </div>
+           </div>
+       </section>
+       <!-- Fin card -->
 
-    <main>
+       <!-- debut section rejoindre -->
+       <div class="mt">
+           <section id="rejoindre">
+               <div class="container">
+                   <article>
+                       <h2 class="br taille">Qu'est ce que vous attendez pour nous rejoindre ?</h2>
+                       <p class="tt">Rejoignez-nous dès maintenant pour profiter d'une expérience unique ! Ne manquez pas nos
+                           événements exclusifs, où vous
+                           pourrez vivre des moments inoubliables, rencontrer de nouvelles personnes et découvrir de
+                           nouvelles passions.
+                           Inscrivez-vous dès aujourd'hui pour réserver vos billets et faire partie de notre communauté
+                           dynamique.</p>
+                       <a href="/register" class="btnb blanc">Je m'inscris</a>
+                   </article>
+           </section>
+       </div>
+       <!-- fin section rejoindre -->
 
-        <section id="programs" class="container">
-            <div class="centered-images">
-                <a href="#" style="background-image: url('img/soiree.jpg');">
-                    <h2 class="br">Soirée au Loft</h2>
-                </a>
-                <a href="#" style="background-image: url('img/course.jpg');">
-                    <h2 class="br">Sortie course à pieds</h2>
-                </a>
-            </div>
-        </section>
-        <!-- fin section #programs -->
-
-        <section id="objectives">
-            <div class="container">
-                <h2 class="br bleu">Nos meilleures sorties :</h2>
-                <p class="br bleu">Ici vous retrouverez nos actions en vedette:</p>
-            </div>
-
-            <!-- fin section #objectives -->
-
-            <!-- Début card -->
-            <div class="imgc">
-                <div class="card-hover">
-                    <div class="card-hover__content">
-                        <h3 class="card-hover__title">
-                            Azard Carnavage Hosted by <span>DJ MARTIN</span>
-                        </h3>
-                        <p class="card-hover__text">Préparez-vous à vous envoler dans l'univers de notre nouvelle soirée
-                            astronomique au AZARD</p>
-                        <a href="/events" class="card-hover__link">
-                            <span>Réserver</span>
-                            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                            </svg>
-                        </a>
-                    </div>
-                    <div class="card-hover__extra">
-                        <h4>Rendez-vous le 9 <span>now</span> and get <span>40%</span> discount!</h4>
-                    </div>
-                    <img src="img/cartes.jpg" alt="">
-                </div>
-                <div class="card-hover">
-                    <div class="card-hover__content">
-                        <h3 class="card-hover__title">
-                            Make your <span>choice</span> right now!
-                        </h3>
-                        <p class="card-hover__text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia
-                            quisquam
-                            doloremque nostrum laboriosam, blanditiis libero corporis nulla a aut?</p>
-                        <a href="/events" class="card-hover__link">
-                            <span>Learn How</span>
-                            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                            </svg>
-                        </a>
-                    </div>
-                    <div class="card-hover__extra">
-                        <h4>Learn <span>now</span> and get <span>40%</span> discount!</h4>
-                    </div>
-                    <img src="img/cartes.jpg" alt="">
-                </div>
-                <div class="card-hover">
-                    <div class="card-hover__content">
-                        <h3 class="card-hover__title">
-                            Make your <span>choice</span> right now!
-                        </h3>
-                        <p class="card-hover__text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia
-                            quisquam
-                            doloremque nostrum laboriosam, blanditiis libero corporis nulla a aut?</p>
-                        <a href="/events" class="card-hover__link">
-                            <span>Learn How</span>
-                            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                            </svg>
-                        </a>
-                    </div>
-                    <div class="card-hover__extra">
-                        <h4>Learn <span>now</span> and get <span>40%</span> discount!</h4>
-                    </div>
-                    <img src="img/cartes.jpg" alt="">
-                </div>
-            </div>
-            </div>
-        </section>
-        <!-- Fin card -->
-
-        <!-- debut section rejoindre -->
-        <div class="mt">
-            <section id="rejoindre">
-                <div class="container">
-                    <article>
-                        <h2 class="br taille">Qu'est ce que vous attendez pour nous rejoindre ?</h2>
-                        <p class="tt">Rejoignez-nous dès maintenant pour profiter d'une expérience unique ! Ne manquez pas nos
-                            événements exclusifs, où vous
-                            pourrez vivre des moments inoubliables, rencontrer de nouvelles personnes et découvrir de
-                            nouvelles passions.
-                            Inscrivez-vous dès aujourd'hui pour réserver vos billets et faire partie de notre communauté
-                            dynamique.</p>
-                        <a href="/register" class="btnb blanc">Je m'inscris</a>
-                    </article>
-            </section>
-        </div>
-        <!-- fin section rejoindre -->
-
-        <div class="mt">
-            <section id="produits" class="container shadow">
-                <h2 class="br">Mieux nous connaître</h2>
-                <ul class="row center-images">
-                    <li class="col-md-4 col-sm-12">
-                        <img src="img/homme.png" alt="">
-                        <h3>Maxime</h3>
-                        <span class="rockwell">Personne 1</span>
-                    </li>
-                    <li class="col-md-4 col-sm-12">
-                        <img src="img/femme.png" alt="">
-                        <h3>Manon Plazza</h3>
-                        <span class="rockwell">Personne 2</span>
-                    </li>
-                    <li class="col-md-4 col-sm-12">
-                        <img src="img/homme2.png" alt="">
-                        <h3>Matéo Grand</h3>
-                        <span class="rockwell">Personne 3</span>
-                    </li>
-                </ul>
-            </section>
-        </div>
+       <div class="mt">
+           <section id="produits" class="container shadow">
+               <h2 class="br">Mieux nous connaître</h2>
+               <ul class="row center-images">
+                   <li class="col-md-4 col-sm-12">
+                       <img src="img/homme.png" alt="">
+                       <h3>Maxime</h3>
+                       <span class="rockwell">Personne 1</span>
+                   </li>
+                   <li class="col-md-4 col-sm-12">
+                       <img src="img/femme.png" alt="">
+                       <h3>Manon Plazza</h3>
+                       <span class="rockwell">Personne 2</span>
+                   </li>
+                   <li class="col-md-4 col-sm-12">
+                       <img src="img/homme2.png" alt="">
+                       <h3>Matéo Grand</h3>
+                       <span class="rockwell">Personne 3</span>
+                   </li>
+               </ul>
+           </section>
+       </div>
 
 
 
-        <!-- debut maps -->
-        <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2785.0917463244234!2d4.851989615837173!3d45.76657597910532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4e0aef04fb3cb%3A0xe4a0b8de9a0367e8!2s53%20Cours%20Albert%20Thomas%2C%2069003%20Lyon!5e0!3m2!1sen!2sfr!4v1607263661843!5m2!1sen!2sfr"
-            width="100%" height="450" frameborder="0" style="border:0;display:block" allowfullscreen=""
-            aria-hidden="false" tabindex="0"></iframe>
+       <!-- debut maps -->
+       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2785.0917463244234!2d4.851989615837173!3d45.76657597910532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4e0aef04fb3cb%3A0xe4a0b8de9a0367e8!2s53%20Cours%20Albert%20Thomas%2C%2069003%20Lyon!5e0!3m2!1sen!2sfr!4v1607263661843!5m2!1sen!2sfr" width="100%" height="450" frameborder="0" style="border:0;display:block" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 
-        <!-- fin maps -->
-    </main>
-    <!-- fin main -->
+       <!-- fin maps -->
+   </main>
+   <!-- fin main -->
 
 
-    <!-- debut footer-->
-    <footer>
-        <div class="container">
-            <article>
-                <img src="img/logo.jpg  " alt="">
-                <p>Suivez-nous sur les réseaux sociaux pour rester à jour avec nos dernières actualités, événements et
-                    offres spéciales!</p>
-                <a href="https://www.facebook.com/bdesciencesulyon/?locale=fr_FR" target="_blank"><i
-                        class="fab fa-facebook"></i></a>
-                <a href="https://www.instagram.com/beede.lyon/" target="_blank"><i class="fab fa-instagram"></i></a>
-                <a href="http://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
-            </article>
-            <form action="">
-                <h2>Nous contacter</h2>
-                <p>Nous sommes là pour vous aider! Que ce soit pour des questions, des suggestions ou simplement pour
-                    dire bonjour,
-                    n'hésitez pas à nous contacter. Notre équipe est disponible pour répondre à vos besoins et vous
-                    offrir le meilleur
-                    service possible.</p>
-                <input type="text" placeholder="Nom">
-                <input type="email" placeholder="Email">
-                <input type="submit" value="Envoyer" class="btnb blanc">
-            </form>
-            <ul>
-                <li>
-                    <i class="fas fa-map-marker-alt"></i>
-                    <div>
-                        <h3>Adresse</h3>
-                        <p>53 Cr Albert Thomas<br>69003 Lyon</p>
-                    </div>
-                </li>
-                <li>
-                    <i class="fas fa-phone-alt"></i>
-                    <div>
-                        <h3>Téléphone</h3>
-                        <p>+33 7 69 09 11 21</p>
-                    </div>
-                </li>
-                <li>
-                    <i class="fas fa-envelope"></i>
-                    <div>
-                        <h3>Mail</h3>
-                        <p>beedelyon@gmail.com</p>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </footer>
-
-</body>
-
-</html>
+   <!-- debut footer-->
+   <footer>
+       <div class="container">
+           <article>
+               <img src="img/logo.jpg  " alt="">
+               <p>Suivez-nous sur les réseaux sociaux pour rester à jour avec nos dernières actualités, événements et
+                   offres spéciales!</p>
+               <a href="https://www.facebook.com/bdesciencesulyon/?locale=fr_FR" target="_blank"><i class="fab fa-facebook"></i></a>
+               <a href="https://www.instagram.com/beede.lyon/" target="_blank"><i class="fab fa-instagram"></i></a>
+               <a href="http://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+           </article>
+           <form action="">
+               <h2>Nous contacter</h2>
+               <p>Nous sommes là pour vous aider! Que ce soit pour des questions, des suggestions ou simplement pour
+                   dire bonjour,
+                   n'hésitez pas à nous contacter. Notre équipe est disponible pour répondre à vos besoins et vous
+                   offrir le meilleur
+                   service possible.</p>
+               <input type="text" placeholder="Nom">
+               <input type="email" placeholder="Email">
+               <input type="submit" value="Envoyer" class="btnb blanc">
+           </form>
+           <ul>
+               <li>
+                   <i class="fas fa-map-marker-alt"></i>
+                   <div>
+                       <h3>Adresse</h3>
+                       <p>53 Cr Albert Thomas<br>69003 Lyon</p>
+                   </div>
+               </li>
+               <li>
+                   <i class="fas fa-phone-alt"></i>
+                   <div>
+                       <h3>Téléphone</h3>
+                       <p>+33 7 69 09 11 21</p>
+                   </div>
+               </li>
+               <li>
+                   <i class="fas fa-envelope"></i>
+                   <div>
+                       <h3>Mail</h3>
+                       <p>beedelyon@gmail.com</p>
+                   </div>
+               </li>
+           </ul>
+       </div>
+   </footer>
