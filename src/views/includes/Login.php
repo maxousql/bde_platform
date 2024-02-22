a4e6be6263a46b586ea7f389d177495949690f8b
maxousql <maxlaiyio@outlook.fr>
2024-02-22 20:27:44 +0100

    ajt msg d'erreur login

--- src/views/includes/Login.php
+++ src/views/includes/Login.php
@@ -4,16 +4,18 @@
         <!-- form -->
         <div class="md:w-1/2 px-8 md:px-16">
             <h2 class="font-bold text-2xl text-[#002D74]">Connexion</h2>
-            <p class="text-xs mt-4 text-[#002D74]">Si vous êtes déjà membre, connectez-vous facilement</p>
+            <p class="text-xs mt-4 mb-4 text-[#002D74]">Si vous êtes déjà membre, connectez-vous facilement</p>
 
             <form method="POST" action="/process_login" class="flex flex-col gap-4">
-                <input class="p-2 mt-8 rounded-xl border" type="email" name="email" id="email" placeholder="Adresse email">
+                <?php if ($_GET['error'] === 'user_not_found') {
+                    echo '<p class="text-xs text-[#002D74]">Cette adresse mail est invalide !</p>';
+                } ?>
+                <input class="p-2 rounded-xl border" type="email" name="email" id="email" placeholder="Adresse email">
                 <div class="relative">
+                    <?php if ($_GET['error'] === 'password') {
+                        echo '<p class="text-xs mb-4 text-[#002D74]">Mot de passe incorrect !</p>';
+                    } ?>
                     <input class="p-2 rounded-xl border w-full" type="password" name="password" id="password" placeholder="Mot de passe">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" class="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2" viewBox="0 0 16 16">
-                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
-                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
-                    </svg>
                 </div>
                 <button class="bg-[#002D74] rounded-xl text-white py-2 hover:scale-105 duration-300" name="valid_login">Connexion</button>
             </form>
