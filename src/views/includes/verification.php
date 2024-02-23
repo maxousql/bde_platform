7667c2cf9991ca54ad2ffbe892c2e5ed7dcb5fec
Yoann VINCENT <yvincent@myges.fr>
2024-02-23 10:47:50 +0100

    page de vérification de mail

---
+++ src/views/includes/verification.php
@@ -0,0 +1,11 @@
+ <section class="bg-gray-50 min-h-screen-custom flex items-center justify-center" style=" padding-top: 20px; ">
+     <!-- login container -->
+     <div class="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
+         <!-- form -->
+         <div class="px-8 md:px-16">
+             <h2 class="font-bold text-2xl text-[#002D74]">Votre adresse mail à bien été vérifié</h2>
+             <p class="text-xs mt-4 text-[#002D74]">Vous faite désormais partis de la plateforme des étudiants du BEEDE Lyon !</p>
+         </div>
+
+     </div>
+ </section>
